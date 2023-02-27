e6cdf55d97f3e85ab17c925bca25241bd6e8cfda
Graham Russell <Graham.Russell@SSE.com>
2023-02-27 10:13:33 +0000

    Still WIP

--- SolarBattery/Program.cs
+++ SolarBattery/Program.cs
@@ -5,16 +5,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        string solarFilename = @"C:\energy\AllSolar.csv";
-        string energyFilename = @"C:\energy\AllEnergy.csv";
+        string solarDirectory = @"C:\energy\solar\";
+        string filesPrefix = "2022";
+        string electrictyDirectory = @"C:\energy\electricty\";
         decimal batteryMaximum = 8.2M;
-        DateTime start = new DateTime(2021,1,1);
-        decimal priceperKwHinPounds = 0.2846M;
-        decimal newpriceperKwHinPounds = 0.340M;
+        DateTime start = new DateTime(2022, 1, 1);
+        decimal priceperKwHinPounds = 0.3427m;
+        decimal newpriceperKwHinPounds = 0.4798m;
         decimal extraWinterUsage = 1.5M; //1.5M;
         Dictionary<string, decimal> pricecaps = new Dictionary<string, decimal>()
         {
-            { "2010", 0.127m }, 
+            { "2010", 0.127m },
             { "2011", 0.137m },
             { "2012", 0.145m },
             { "2013", 0.152m },
@@ -27,16 +28,18 @@ class Program
             { "2020", 0.196m },
             { "2021", 0.212m },
             { "2022", 0.2846m },
-            { "2022+", 0.340m }
+            { "2023", 0.3427m }
         };
 
-        Console.WriteLine("Reading Solar file");
-        Dictionary<string,string> solar = new Dictionary<string, string>();
-        solar = LoadFile(solarFilename);
+        Dictionary<string, string> solar;
+        Dictionary<string, string> energy;
 
-        Console.WriteLine("Reading energy file");
-        Dictionary<string, string> energy = new Dictionary<string, string>();
-        energy = LoadFile(energyFilename);
+        Console.WriteLine("Reading all solar files in {0} beginning with {1}", solarDirectory, filesPrefix);
+        solar = LoadFilesInDirectory(solarDirectory, filesPrefix, ";");
+
+        Console.WriteLine("Reading all electricity files in {0} beginning with {1}", electrictyDirectory, filesPrefix);
+        energy = LoadFilesInDirectory(electrictyDirectory, filesPrefix, ",");
+
 
         DateTime processingDate = start;
         decimal batteryLevelkWh = 0.0M;
@@ -44,22 +47,27 @@ class Program
         decimal totalUsedWithoutBattery = 0.0M;
         decimal batterySavedkWh = 0.0M;
         decimal totalBatterySavedkWh = 0.0M;
-        while (processingDate < new DateTime(2022,1,1))
+        while (processingDate < new DateTime(2023, 1, 1))
         {
             batteryLevelkWh += decimal.Parse(solar[processingDate.ToString("dd/MM/yyyy")]);
-            if (batteryLevelkWh > batteryMaximum) { batteryLevelkWh = batteryMaximum;  }
+            if (batteryLevelkWh > batteryMaximum) { batteryLevelkWh = batteryMaximum; }
             usedGridkWh = decimal.Parse(energy[processingDate.ToString("dd/MM/yyyy")]);
 
-            if (processingDate.Month >= 10 || processingDate.Month <= 4) {
+            if (processingDate.Month >= 10 || processingDate.Month <= 4)
+            {
                 usedGridkWh += extraWinterUsage;
             };
 
-            if (batteryLevelkWh >= usedGridkWh) { 
-                batterySavedkWh = usedGridkWh; } 
-            else { batterySavedkWh = batteryLevelkWh; 
+            if (batteryLevelkWh >= usedGridkWh)
+            {
+                batterySavedkWh = usedGridkWh;
+            }
+            else
+            {
+                batterySavedkWh = batteryLevelkWh;
             };
 
-            batteryLevelkWh-= usedGridkWh;
+            batteryLevelkWh -= usedGridkWh;
             if (batteryLevelkWh < 0.0M) { batteryLevelkWh = 0.0M; }
 
             totalUsedWithoutBattery += usedGridkWh;
@@ -69,9 +77,9 @@ class Program
         }
 
         Console.WriteLine("Total used from grid without battery (in year) {0}.  With the current price tariff that's £{1}", totalUsedWithoutBattery.ToString("0.00"), (totalUsedWithoutBattery * priceperKwHinPounds).ToString("0.##"));
-        Console.WriteLine("Total used from grid with battery (in year) {0}", (totalUsedWithoutBattery-totalBatterySavedkWh).ToString("0.00"));
+        Console.WriteLine("Total used from grid with battery (in year) {0}", (totalUsedWithoutBattery - totalBatterySavedkWh).ToString("0.00"));
         Console.WriteLine("Battery saved {0} kWh over the year", totalBatterySavedkWh.ToString("0.00"));
-        Console.WriteLine("Thats £{0} with the current price of {1}", (totalBatterySavedkWh * priceperKwHinPounds).ToString("0.00"), priceperKwHinPounds.ToString("0.00") );
+        Console.WriteLine("Thats £{0} with the current price of {1}", (totalBatterySavedkWh * priceperKwHinPounds).ToString("0.00"), priceperKwHinPounds.ToString("0.00"));
         Console.WriteLine("With the new tariff that would be £{0} saved.", (totalBatterySavedkWh * newpriceperKwHinPounds).ToString("0.00"));
 
         decimal runningTotal = 0.0m;
@@ -81,9 +89,9 @@ class Program
             runningTotal += pricecap.Value * totalBatterySavedkWh;
         }
         Console.WriteLine("  That's a total of £{0}", runningTotal.ToString("0.00"));
-    }   
+    }
 
-    static public Dictionary<string, string> LoadFile(string filename)
+    static public Dictionary<string, string> LoadFile(string filename, string delimeter)
     {
         Dictionary<string, string> filecontents = new Dictionary<string, string>();
 
@@ -92,13 +100,69 @@ class Program
             while (!reader.EndOfStream)
             {
                 var line = reader.ReadLine();
-                if (line is not null) {
-                    var values = line.Split(',');
+                if (line is not null)
+                {
+                    var values = line.Split(delimeter);
 
-                    filecontents.Add(values[0], values[1]);
+                    DateTime enteredDate = DateTime.Parse(values[0]);
+
+                    filecontents.Add(enteredDate.ToString("dd/MM/yyyy"), values[1]);
                 }
             }
         }
+        return filecontents;
+    }
+
+    static public Dictionary<string, string> LoadFile(string filename, string delimeter, Dictionary<string, string> filecontents)
+    {
+        //        Dictionary<string, string> filecontents = new Dictionary<string, string>();
+
+        using (var reader = new StreamReader(filename))
+        {
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                if (line is not null)
+                {
+                    // Skip header row
+                    if (line.StartsWith(" ;") || line.StartsWith('"')) { }
+                    else
+                    {
+                        var values = line.Split(delimeter);
+
+//                        filecontents.Add(values[0], values[1]);
+                        DateTime enteredDate = DateTime.Parse(values[0]);
+
+                        filecontents.Add(enteredDate.ToString("dd/MM/yyyy"), values[1]);
+
+                    }
+                }
+            }
+        }
+        return filecontents;
+    }
+
+
+    static public Dictionary<string, string> LoadFilesInDirectory(string directory, string prefix, string delimeter)
+    {
+
+        Dictionary<string, string> filecontents = new Dictionary<string, string>();
+        //        Dictionary<string, string> allfiles = new Dictionary<string, string>();
+
+        string[] fileEntries = Directory.GetFiles(directory);
+        string tmp;
+        foreach (string filename in fileEntries)
+        {
+
+            tmp = filename.Replace(directory, "");
+
+            if (tmp.StartsWith(prefix))
+            {
+                Console.WriteLine("Reading file {0}", filename);
+                filecontents = LoadFile(filename, delimeter, filecontents);
+            }
+        }
+
         return filecontents;
     }
 }
